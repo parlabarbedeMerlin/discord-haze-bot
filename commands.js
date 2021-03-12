380d5522621df44b8d6cd642ac4383b08803e3ae
Alexiscomete <63195504+Alexiscomete@users.noreply.github.com>
2021-03-12 21:28:29 +0100

    Ajout de mon fichier command 🙃

---
+++ commands.js
@@ -0,0 +1,53 @@
+const Discord = require('discord.js');
+const index = require("./index");
+
+/**
+ * Liste des commandes
+ * @type {Map<string, (message: Discord.Message, args: string[], content: string) => void>}
+ */
+const commands = new Map();
+
+/**
+ * Ajouter une commande
+ * @param {(message: Discord.Message, args: string[], content: string) => void} commande 
+ * @param {string} name
+ */
+function addCommand(name, commande) {
+    commands.set(name, commande);
+}
+
+/*
+Regarde les msg
+ */
+index.bot.on("message", (msg) => {
+    let content = msg.content.toLowerCase();
+    if (content.startsWith("$")) { //exemple 1 de préfix
+        check(msg, content, 1); 
+    }else if (content.startsWith("ABC")) { //exemple 2 de préfix
+        check(msg, content, 1);
+    }
+});
+
+function check(msg, content, size) {
+    //séparation des arguments et supression du préfix
+    let commande = content.slice(size);
+    let args = commande.split(" ");
+    commande = args[0];
+    let commandFunction = commands.get(commande);
+        if (commandFunction != undefined) {
+            //supression de la commande
+            if (msg.deletable) msg.delete();
+            //exécution
+            commandFunction(msg, args, content);
+        } else {
+            // message d'erreur
+            msg.channel.send("Cette commande n'existe pas ...\nLa commande indiquée est : " + commande + " \nLes abréviations ne sont pas acceptées").then(message => {
+                if (message.deletable)
+                    message.delete({ timeout: 50000 });
+            });
+        }
+}
+
+module.exports = {
+    addCommand,
+}
